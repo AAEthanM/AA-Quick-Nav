50ae59d52e3f93849bd5a82a9fb43621050a61e9
AAEthanM <116016642+AAEthanM@users.noreply.github.com>
2022-11-08 15:43:28 -0500

    Update AA EMTS Dashboard Adjuster.user.js

--- AA EMTS Dashboard Adjuster.user.js
+++ AA EMTS Dashboard Adjuster.user.js
@@ -4,6 +4,8 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
+// @downloadURL  https://github.com/AAEthanM/AA-Quick-Nav/raw/main/AA%20EMTS%20Dashboard%20Adjuster.user.js
+// @updateURL    https://github.com/AAEthanM/AA-Quick-Nav/raw/main/AA%20EMTS%20Dashboard%20Adjuster.user.js
 // @match        https://webapp.brightmetrics.com/UI-2/pages/Dashboard.aspx?dashtoken=3b760263-9d0b-4dfc-a4a2-0b716bafb205/3ae5c019-1390-4f1b-93dd-46f4e065cbb7
 // @icon         https://cdn.worldvectorlogo.com/logos/assa.svg
 // @grant        none
@@ -11,44 +13,52 @@
 
 (function() {
     'use strict';
-        window.setTimeout(adjust,5000);
+
+    var changesHeight = [
+        ["graph8",368],
+        ["chart_l83glbs1-ej9pm0fe3b",132],
+        ["l83glbs1-ej9pm0fe3b",132],
+        ["graph1",500],
+        ["graph2",500],
+        ["graph18",246],
+        ["graph19",279],
+    ];
+    var changesTier2 = [
+        "-100px", //Tier2
+        "-196px", //Daily Group Stats
+        "-196px", //Call Queues
+        "-130px", //TPS New Haven
+        "-130px", //TPS Berlin
+    ];
+
+    window.setTimeout(adjust,5000);
+    window.setTimeout(adjust,10000);
+
     function adjust() {
-        var chart = document.getElementById("graph8");
-        chart.style.height = "368px";
+        for(let i = 0; i < changesHeight.length; i++) {
+            console.log(changesHeight[i][0]);
+            var elm = document.getElementById(changesHeight[i][0].toString());
+            elm.style.height = changesHeight[i][1]+"px";
+        }
+        for(let i = 1; i <= changesTier2.length; i++) {
+            var tier2spacing = document.getElementsByClassName("ui-selectee");
+            console.log(tier2spacing);
+            tier2spacing[i].style.top = changesTier2[i-1];
+        }
 
-        var tier2 = document.getElementById("chart_l83glbs1-ej9pm0fe3b");
-        tier2.style.height = "132px";
+        var graph18 = document.getElementById("graph18");
+        var graph19 = document.getElementById("graph19");
 
-        var tier2box = document.getElementById("l83glbs1-ej9pm0fe3b");
-        tier2box.style.height = "132px";
+        graph18.style.top = "-10px";
+        graph18.style.position = "relative";
 
-        var tier2spacing = document.getElementsByClassName("ui-selectee");
-        tier2spacing[1].style.top = "-100px";
-        tier2spacing[2].style.top = "-196px"; //Daily Group Stats
-        tier2spacing[3].style.top = "-196px"; //Call Queues
-        tier2spacing[4].style.top = "-130px"; //TPS New Haven
-        tier2spacing[5].style.top = "-130px"; //TPS Berlin
+        graph19.style.top = "-93px";
+        graph19.style.position = "relative";
 
         var pageContent = document.getElementById("page-content");
         pageContent.style.top = "-30px";
-
-        //var chartContent = document.getElementsByClassName("c-dashboard__chart-content");
-        //chartContent[2].style.height = "77%";
-        //chartContent[3].style.height = "77%";
-        //console.log(chartContent);
-
-        var graph1 = document.getElementById("graph1");
-        var graph2 = document.getElementById("graph2");
-        var graph18 = document.getElementById("graph18");
-        var graph19 = document.getElementById("graph19");
-        graph1.style.height = "500px";
-        graph2.style.height = "500px";
-        graph18.style.height = "246px";
-        graph18.style.top = "-10px";
-        graph18.style.position = "relative";
-        graph19.style.height = "279px";
-        graph19.style.top = "-93px";
-        graph19.style.position = "relative";
+        pageContent.style.left = "-30px";
+        pageContent.style.width = "102.1%";
 
         var colorbar = document.getElementsByClassName("c-dashboard__chart-color-bar");
         for(let i = 0; i < colorbar.length; i++) {
